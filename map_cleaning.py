dc3ac624657374e07d890708a23f0fdb562a35c1
mkrug6 <82290424+mkrug6@users.noreply.github.com>
2021-04-24 10:59:25 -0700

    Update map_cleaning.py

--- map_cleaning.py
+++ map_cleaning.py
@@ -1,12 +1,13 @@
-osmfile = 'wlinn'
-
 import xml.etree.cElementTree as ET
+from collections import defaultdict
+import re
 
-expected_values = ['Avenue', 'Alley', 'Road', 'Street', 'Trail', 'Landing', 'Pointe', 
-                   'Vista', 'Woods', 'Curve', 'Path', 'Freeway', 'Grotto', 'Court', 
-                   'Northbound', 'Southbound', 'Drive', 'Boulevard', 'Lane', 'Circle',
-                   'Highway', 'Place', 'Loop', 'Terrace', 'Way', 'Crest', 'Parkway',
-                   'Point']
+osmfile = 'wlinn_sample'
+#typo_full_names = {}
+#corrected_names = {}      
+street_type_re = re.compile(r'\S+\.?$', re.IGNORECASE)
+expected_values = ['Avenue', 'Alley', 'Road', 'Street', 'Trail', 'Landing', 'Pointe', 'Vista', 'Woods', 'Curve', 'Path', 'Freeway', 'Grotto', 'Court', 'Northbound', 'Southbound', 'Drive', 'Boulevard', 'Lane', 'Circle', 'Highway', 'Place', 'Loop', 'Terrace', 'Way', 'Crest', 'Parkway', 'Point', 'View', 'Commons', 'Run', 'South', 'North', 'East', 'Circus', 'Summit', 'West', '99E', '224', '213', 'View', '212', 'Downs']
+
 
 abbr_mapping = {'Ave': 'Avenue',
                 'TRL': 'Trail',
@@ -29,52 +30,6 @@ abbr_mapping = {'Ave': 'Avenue',
                 'Street.': 'Street'
                 }
 
-typo_full_names = {}
-
-def audit_street_name(street_types, street_name):
-    m = street_type_re.search(street_name)
-    if m:
-        street_type = m.group()
-        if (all_types[street_type] < 20) and (street_type not in expected_values) and (street_type not in abbr_mapping):
-            if street_type in typo_full_names:
-                typo_full_names[street_type].append(street_name)
-            else:
-                typo_full_names.update({ street_type:[street_name] })
-
-def audit_name(filename):
-    for event, elem in ET.iterparse(filename):
-        if is_street_name(elem):
-            audit_street_name(street_types, elem.attrib['v'])    
-    return typo_full_names
-
-audit_name(osmfile)
-
-
-expected_values.extend([
-    'View', 'Commons', 'Run', 'South', 'North', 'East', 'Circus', 'Summit', 'West', 
-    '99E', '224', '213', 'View', '212', 'Downs'
-                       ])
-
-
-name_fix = { 
-                'Boticelli': 'Boticelli Street',
-                'Southwest Wheatland': 'Southwest Wheatland Road',
-                'Falstaff': 'Falstaff Road',
-                'Pimlico': 'Pimlico Drive',
-                'Hotspur': 'Hotspur Road',
-                'Southwest Miami': 'Southwest Miami Street',
-                'Pericles': 'Pericles Loop',
-                'Polonius': 'Polonius Loop',
-                'El Greco': 'El Greco Street',
-                'Wheatherstone': 'Wheatherstone Street',
-                '8202 SE Flavel St, Portland, OR 97266': 'SE Flavel Street',
-                'Cervantes': 'Cervantes Street',
-                'Touchstone': 'Touchstone Road',
-                'Polonius': 'Polonius Street',
-                'Spinosa': 'Spinosa Road',
-                'Southeast Fieldcrest': 'Southeast Fieldcrest Road'
-               }
-
 spelling_fix = { 
                 'Falstaff': 'Falstaff Road',
                 'Pimlico': 'Pimlico Drive',
@@ -89,11 +44,39 @@ spelling_fix = {
                 'Spinosa': 'Spinosa Road',
                }
 
-# Let's sort our expected variable to aid in readability
+def is_street_name(elem):
+    return (elem.tag == "tag") and (elem.attrib['k'] == "addr:street")
+  
+def print_sorted_dict(d, expression):
+    keys = d.keys()
+    keys = sorted(keys, key=lambda s: s.lower())
+    for k in keys:
+        v = d[k]
+        print (expression % (k, v))
 
-expected_values = sorted(expected_values)
-expected_values
+'''
+  
 
+ 
+def audit_street_name(street_types, street_name):
+    m = street_type_re.search(street_name)
+    if m:
+        street_type = m.group()
+        if (all_types[street_type] < 20) and (street_type not in expected_values) and (street_type not in abbr_mapping):
+            if street_type in typo_full_names:
+                typo_full_names[street_type].append(street_name)
+            else:
+                typo_full_names.update({ street_type:[street_name] })
+'''
+
+'''
+
+def audit_name(filename):
+    for event, elem in ET.iterparse(filename):
+        if is_street_name(elem):
+            audit_street_name(street_types, elem.attrib['v'])    
+    return typo_full_names
+'''
 
 def update_name(name):
     street_type = name.split(' ')[-1]
@@ -145,7 +128,10 @@ def run_updates(filename):
             if better_name != name:
                 corrected_names[name] = better_name
     return corrected_names
-            
-corrected_names = {}           
-corrected_names = run_updates(osmfile)
-print_sorted_dict(corrected_names, "%s: %s")
+
+def clean_map(osmfile):
+    expected_values = ['Avenue', 'Alley', 'Road', 'Street', 'Trail', 'Landing', 'Pointe', 'Vista', 'Woods', 'Curve', 'Path', 'Freeway', 'Grotto', 'Court', 'Northbound', 'Southbound', 'Drive', 'Boulevard', 'Lane', 'Circle', 'Highway', 'Place', 'Loop', 'Terrace', 'Way', 'Crest', 'Parkway', 'Point', 'View', 'Commons', 'Run', 'South', 'North', 'East', 'Circus', 'Summit', 'West', '99E', '224', '213', 'View', '212', 'Downs']
+    expected_values = sorted(expected_values)
+    corrected_names = run_updates(osmfile)
+    return print_sorted_dict(corrected_names, "%s: %s")
+
