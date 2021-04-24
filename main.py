709a5a2efecc462a7b1362bf44ba1e2a3c5aa0f9
mkrug6 <82290424+mkrug6@users.noreply.github.com>
2021-04-24 10:58:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import audit.py
-import map_cleaning.py
+from map_cleaning import *
+from audit import *
+from create_csvs import *
+from theschema import *
+import re
 
-osmfile = 'wlinn_sample'
+mapfile = osmfile
+expression = ""
 
+
+#Runs the main function in audit.py
+audit(mapfile)
+
+#Runs the main function in map_cleaning.py
+clean_map(mapfile)
+
+#Makes the CSV files
+process_map(osmfile, validate=False)
