9e8d351805bc6dc98afa4cceca0eccc19cb71d83
mkrug6 <82290424+mkrug6@users.noreply.github.com>
2021-04-24 10:58:53 -0700

    Create create_csvs.py

---
+++ create_csvs.py
@@ -0,0 +1,234 @@
+import csv
+import codecs
+import re
+import xml.etree.cElementTree as ET
+import cerberus
+#import schema
+
+theschema = {
+    'node': {
+        'type': 'dict',
+        'schema': {
+            'id': {'required': True, 'type': 'integer', 'coerce': int},
+            'lat': {'required': True, 'type': 'float', 'coerce': float},
+            'lon': {'required': True, 'type': 'float', 'coerce': float},
+            'user': {'required': True, 'type': 'string'},
+            'uid': {'required': True, 'type': 'integer', 'coerce': int},
+            'version': {'required': True, 'type': 'string'},
+            'changeset': {'required': True, 'type': 'integer', 'coerce': int},
+            'timestamp': {'required': True, 'type': 'string'}
+        }
+    },
+    'node_tags': {
+        'type': 'list',
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'id': {'required': True, 'type': 'integer', 'coerce': int},
+                'key': {'required': True, 'type': 'string'},
+                'value': {'required': True, 'type': 'string'},
+                'type': {'required': True, 'type': 'string'}
+            }
+        }
+    },
+    'way': {
+        'type': 'dict',
+        'schema': {
+            'id': {'required': True, 'type': 'integer', 'coerce': int},
+            'user': {'required': True, 'type': 'string'},
+            'uid': {'required': True, 'type': 'integer', 'coerce': int},
+            'version': {'required': True, 'type': 'string'},
+            'changeset': {'required': True, 'type': 'integer', 'coerce': int},
+            'timestamp': {'required': True, 'type': 'string'}
+        }
+    },
+    'way_nodes': {
+        'type': 'list',
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'id': {'required': True, 'type': 'integer', 'coerce': int},
+                'node_id': {'required': True, 'type': 'integer', 'coerce': int},
+                'position': {'required': True, 'type': 'integer', 'coerce': int}
+            }
+        }
+    },
+    'way_tags': {
+        'type': 'list',
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'id': {'required': True, 'type': 'integer', 'coerce': int},
+                'key': {'required': True, 'type': 'string'},
+                'value': {'required': True, 'type': 'string'},
+                'type': {'required': True, 'type': 'string'}
+            }
+        }
+    }
+}
+
+OSM_PATH = "osmfile"
+corrected_names = {} 
+NODES_PATH = "nodes.csv"
+NODE_TAGS_PATH = "nodes_tags.csv"
+WAYS_PATH = "ways.csv"
+WAY_NODES_PATH = "ways_nodes.csv"
+WAY_TAGS_PATH = "ways_tags.csv"
+
+LOWER_COLON = re.compile(r'^([a-z]|_)+:([a-z]|_)+')
+PROBLEMCHARS = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+# Make sure the fields order in the csvs matches the column order in the sql table schema
+NODE_FIELDS = ['id', 'lat', 'lon', 'user', 'uid', 'version', 'changeset', 'timestamp']
+NODE_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_FIELDS = ['id', 'user', 'uid', 'version', 'changeset', 'timestamp']
+WAY_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_NODES_FIELDS = ['id', 'node_id', 'position']
+
+def correct_element(v):
+    if v in corrected_names:
+        correct_value = corrected_names[v]
+    else:
+        correct_value = v
+    return correct_value
+
+def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIELDS,
+                  problem_chars=PROBLEMCHARS, default_tag_type='regular'):
+    """Clean and shape node or way XML element to Python dict"""
+
+    node_attribs = {}
+    way_attribs = {}
+    way_nodes = []
+    tags = []  # Handle secondary tags the same way for both node and way elements
+
+    if element.tag == 'node':
+        node_attribs['id'] = element.attrib['id']
+        node_attribs['user'] = element.attrib['user']
+        node_attribs['uid'] = element.attrib['uid']
+        node_attribs['version'] = element.attrib['version']
+        node_attribs['lat'] = element.attrib['lat']
+        node_attribs['lon'] = element.attrib['lon']
+        node_attribs['timestamp'] = element.attrib['timestamp']
+        node_attribs['changeset'] = element.attrib['changeset']
+        
+        for node in element:
+            tag_dict = {}
+            tag_dict['id'] = element.attrib['id']
+            if ':' in node.attrib['k']:
+                tag_dict['type'] = node.attrib['k'].split(':', 1)[0]
+                tag_dict['key'] = node.attrib['k'].split(':', 1)[-1]
+                tag_dict['value'] = correct_element(node.attrib['v'])
+            else:
+                tag_dict['type'] = 'regular'
+                tag_dict['key'] = node.attrib['k']
+                tag_dict['value'] = correct_element(node.attrib['v'])
+            tags.append(tag_dict)
+            
+    elif element.tag == 'way':
+        way_attribs['id'] = element.attrib['id']
+        way_attribs['user'] = element.attrib['user']
+        way_attribs['uid'] = element.attrib['uid']
+        way_attribs['version'] = element.attrib['version']
+        way_attribs['timestamp'] = element.attrib['timestamp']
+        way_attribs['changeset'] = element.attrib['changeset']
+        n = 0
+        for node in element:
+            if node.tag == 'nd':
+                way_dict = {}
+                way_dict['id'] = element.attrib['id']
+                way_dict['node_id'] = node.attrib['ref']
+                way_dict['position'] = n
+                n += 1
+                way_nodes.append(way_dict)
+            if node.tag == 'tag':
+                tag_dict = {}
+                tag_dict['id'] = element.attrib['id']
+                if ':' in node.attrib['k']:
+                    tag_dict['type'] = node.attrib['k'].split(':', 1)[0]
+                    tag_dict['key'] = node.attrib['k'].split(':', 1)[-1]
+                    tag_dict['value'] = correct_element(node.attrib['v'])
+                else:
+                    tag_dict['type'] = 'regular'
+                    tag_dict['key'] = node.attrib['k']
+                    tag_dict['value'] = correct_element(node.attrib['v'])
+                tags.append(tag_dict)
+    
+    if element.tag == 'node':
+        return {'node': node_attribs, 'node_tags': tags}
+    elif element.tag == 'way':
+        return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
+
+# ================================================== #
+#               Helper Functions                     #
+# ================================================== #
+def get_element(osm_file, tags=('node', 'way', 'relation')):
+    """Yield element if it is the right type of tag"""
+
+    context = ET.iterparse(osm_file, events=('start', 'end'))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
+
+def validate_element(element, validator, schema=theschema):
+    """Raise ValidationError if element does not match schema"""
+    if validator.validate(element, schema) is not True:
+        field, errors = next(validator.errors.iteritems())
+        message_string = "\nElement of type '{0}' has the following errors:\n{1}"
+        error_string = pprint.pformat(errors)
+        
+        raise Exception(message_string.format(field, error_string))
+
+class UnicodeDictWriter(csv.DictWriter, object):
+    """Extend csv.DictWriter to handle Unicode input"""
+
+    def writerow(self, row):
+        super(UnicodeDictWriter, self).writerow({
+            k: (v.encode('utf-8') if isinstance(v, str) else v) for k, v in row.items()
+        })
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+# ================================================== #
+#               Main Function                        #
+# ================================================== #
+def process_map(file_in, validate):
+    """Iteratively process each XML element and write to csv(s)"""
+
+    with codecs.open(NODES_PATH, 'w', encoding="utf-8") as nodes_file, \
+    codecs.open(NODE_TAGS_PATH, 'w', encoding="utf-8") as nodes_tags_file, \
+    codecs.open(WAYS_PATH, 'w', encoding="utf-8") as ways_file, \
+    codecs.open(WAY_NODES_PATH, 'w', encoding="utf-8") as way_nodes_file, \
+    codecs.open(WAY_TAGS_PATH, 'w', encoding="utf-8") as way_tags_file:
+
+        nodes_writer = csv.DictWriter(nodes_file, NODE_FIELDS)
+        node_tags_writer = csv.DictWriter(nodes_tags_file, NODE_TAGS_FIELDS)
+        ways_writer = csv.DictWriter(ways_file, WAY_FIELDS)
+        way_nodes_writer = csv.DictWriter(way_nodes_file, WAY_NODES_FIELDS)
+        way_tags_writer = csv.DictWriter(way_tags_file, WAY_TAGS_FIELDS)
+
+        nodes_writer.writeheader()
+        node_tags_writer.writeheader()
+        ways_writer.writeheader()
+        way_nodes_writer.writeheader()
+        way_tags_writer.writeheader()
+
+        validator = cerberus.Validator()
+
+        for element in get_element(file_in, tags=('node', 'way')):
+            el = shape_element(element)
+            if el:
+                if validate is True:
+                    validate_element(el, validator)
+
+                if element.tag == 'node':
+                    nodes_writer.writerow(el['node'])
+                    node_tags_writer.writerows(el['node_tags'])
+                elif element.tag == 'way':
+                    ways_writer.writerow((el['way']))
+                    way_nodes_writer.writerows(el['way_nodes'])
+                    way_tags_writer.writerows(el['way_tags'])
+                    
