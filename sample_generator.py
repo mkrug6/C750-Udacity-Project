ee7cd79d1410430752ea2b9e7aca8f81354b9e1b
mkrug6 <82290424+mkrug6@users.noreply.github.com>
2021-04-25 12:01:09 -0700

    Update sample_generator.py

--- sample_generator.py
+++ sample_generator.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from config import *
 import xml.etree.ElementTree as ET
 
-OSM_FILE = "wlinn"  # Replace this with your osm file
-SAMPLE_FILE = "wlinn"
+#This is a file from Udacity for the map cleaning project with Open Street Map.
+#The bigger the k value, the smaller the output file.
 
 k = 20 # Parameter: take every k-th top level element
 
-def get_element(osm_file, tags=('node', 'way', 'relation')):
+def get_element(osmfile, tags=('node', 'way', 'relation')):
     """Yield element if it is the right type of tag
 
     Reference:
     http://stackoverflow.com/questions/3095434/inserting-newlines-in-xml-file-generated-via-xml-etree-elementtree-in-python
     """
-    context = iter(ET.iterparse(osm_file, events=('start', 'end')))
+    context = iter(ET.iterparse(osmfile, events=('start', 'end')))
     _, root = next(context)
     for event, elem in context:
         if event == 'end' and elem.tag in tags:
@@ -27,7 +27,7 @@ with open(SAMPLE_FILE, 'wb') as output:
     output.write(b'<osm>\n')
 
     # Write every kth top level element
-    for i, element in enumerate(get_element(OSM_FILE)):
+    for i, element in enumerate(get_element(osmfile)):
         if i % k == 0:
             output.write(ET.tostring(element, encoding='utf-8'))
 
